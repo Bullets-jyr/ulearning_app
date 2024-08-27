3accad069a4f53d6eecd29e5f889142b52fb95f8
Bullets-jyr <jyr@bullets.co.kr>
2024-08-27 09:06:57 +0900

    Show dots indicator

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,13 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Welcome(),
+      home: const Welcome(),
     );
   }
 }
 
 final appCount = StateProvider<int>((ref) {
-  return 3;
+  return 1;
 });
 
 class MyHomePage extends ConsumerWidget {
@@ -41,17 +41,10 @@ class MyHomePage extends ConsumerWidget {
     int count = ref.watch(appCount);
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: const Text('Riverpod app'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
--- lib/pages/welcome/welcome.dart
+++ lib/pages/welcome/welcome.dart
@@ -1,58 +1,63 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ulearning_app/pages/welcome/widgets.dart';
 
 class Welcome extends StatelessWidget {
-  Welcome({super.key});
-
-  final PageController _controller = PageController();
+  const Welcome({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final PageController _controller = PageController();
     return Container(
       color: Colors.white,
       child: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.white,
           body: Container(
-            margin: EdgeInsets.only(top: 30.h),
+            margin: EdgeInsets.only(top: 30),
             child: Stack(
+              alignment: Alignment.topCenter,
               children: [
+                // showing our three welcome pages
                 PageView(
                   controller: _controller,
                   scrollDirection: Axis.horizontal,
                   children: [
-                    // first page
-                    appOnboardingPage(
-                      _controller,
-                      imagePath: 'assets/images/reading.png',
-                      title: 'First See Learning',
-                      subTitle: 'Forget about the paper, now learning all in one place',
-                      index: 1,
-                    ),
-                    // second page
-                    appOnboardingPage(
-                      _controller,
-                      imagePath: 'assets/images/man.png',
-                      title: 'Connect With Everyone',
-                      subTitle: 'Always keep in touch with your tutor and friends. Let\'s get connected',
-                      index: 2,
-                    ),
-                    appOnboardingPage(
-                      _controller,
-                      imagePath: 'assets/images/boy.png',
-                      title: 'Always Fascinated Learning',
-                      subTitle: 'Anywhere, anytime. The time is at your discretion. So study wherever you can',
-                      index: 3,
-                    ),
+                    appOnboardingPage(_controller,
+                        imagePath: "assets/images/reading.png",
+                        title: "First See Learning",
+                        subTitle:
+                            "Forget about of paper all knowledge in one learning",
+                        index: 1),
+                    appOnboardingPage(_controller,
+                        imagePath: "assets/images/man.png",
+                        title: "Connect With Everyone",
+                        subTitle:
+                            "Always keep in touch with your tutor and friends. Let's get connected",
+                        index: 2),
+                    appOnboardingPage(_controller,
+                        imagePath: "assets/images/boy.png",
+                        title: "Always Fascinated Learning",
+                        subTitle:
+                            "Anywhere, anytime. The time is at your discretion. So study wherever you can",
+                        index: 3),
                   ],
                 ),
-                const Positioned(
-                  child: Text(
-                    '',
+                // for showing dots
+                Positioned(
+                  bottom: 10,
+                  child: DotsIndicator(
+                    dotsCount: 3,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    decorator: DotsDecorator(
+                      size: const Size.square(9.0),
+                      activeSize: const Size(24.0, 8.0),
+                      activeShape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
                   ),
-                  left: 20,
-                  bottom: 100,
                 ),
               ],
             ),
