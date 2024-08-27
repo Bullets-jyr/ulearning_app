d1c94e3bc22a78502052de0bc8273ca3b70be230
Bullets-jyr <jyr@bullets.co.kr>
2024-08-27 09:43:18 +0900

    Install tools and generate providers

---
+++ lib/pages/notifier/welcome_notifier.dart
@@ -0,0 +1,11 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'welcome_notifier.g.dart';
+
+@riverpod
+class IndexDot extends _$IndexDot {
+  @override
+  int build() {
+    return 0;
+  }
+}
