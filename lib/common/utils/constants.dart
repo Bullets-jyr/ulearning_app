4ea24e661378824a121e45da8c18c4e0a09e9c39
Bullets-jyr <jyr@bullets.co.kr>
2024-09-19 12:08:13 +0900

    Network IP address and Android test

--- lib/common/utils/constants.dart
+++ lib/common/utils/constants.dart
@@ -1,6 +1,8 @@
 class AppConstants {
   // iOS
-  // static const String SERVER_API_URL = 'http:127.0.0.1:8000';
+  // static const String SERVER_API_URL = 'http:127.0.0.1:8000/';
+  // Android
+  // static const String SERVER_API_URL = 'http:10.0.2.2:8000/';
   static const String SERVER_API_URL = 'https://rulearn.dbestech.com/';
   static const String STORAGE_USER_PROFILE_KEY = 'user_profile';
   static const String STORAGE_USER_TOKEN_KEY = 'user_token';
