10a4299f9ecd4fa644d52ee5bc1671209891f7b1
Bullets-jyr <jyr@bullets.co.kr>
2024-09-30 20:11:48 +0900

    Build payment ui

--- lib/features/buy_course/controller/buy_course_controller.dart
+++ lib/features/buy_course/controller/buy_course_controller.dart
@@ -1,21 +1,19 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:ulearning_app/common/models/course_entities.dart';
 import 'package:ulearning_app/features/buy_course/repo/buy_course_repo.dart';
-import 'package:ulearning_app/features/course_detail/repo/course_repo.dart';
 
 part 'buy_course_controller.g.dart';
 
 @riverpod
-Future<String?> buyCourseController(
-    BuyCourseControllerRef ref, {required int index}) async {
+Future<String?> buyCourseController(BuyCourseControllerRef ref,
+    {required int index}) async {
   // ref.watch(homeCourseListProvider);
   CourseRequestEntity courseRequestEntity = CourseRequestEntity();
   courseRequestEntity.id = index;
-  final response =
-  await BuyCourseRepo.buyCourse(params: courseRequestEntity);
+  final response = await BuyCourseRepo.buyCourse(params: courseRequestEntity);
   if (response.code == 200) {
-    // return response.data;
-    return null;
+    return response.data;
+    // return null;
   } else {
     print('request failed ${response.code}');
   }
--- lib/features/buy_course/view/buy_course.dart
+++ lib/features/buy_course/view/buy_course.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ulearning_app/features/buy_course/controller/buy_course_controller.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class BuyCourse extends ConsumerStatefulWidget {
   const BuyCourse({super.key});
@@ -10,7 +12,8 @@ class BuyCourse extends ConsumerStatefulWidget {
 
 class _BuyCourseState extends ConsumerState<BuyCourse> {
   late var args;
-  
+  late WebViewController webViewController;
+
   @override
   void didChangeDependencies() {
     var id = ModalRoute.of(context)!.settings.arguments as Map;
@@ -23,9 +26,31 @@ class _BuyCourseState extends ConsumerState<BuyCourse> {
 
   @override
   Widget build(BuildContext context) {
+    var courseBuy = ref.watch(buyCourseControllerProvider(index: args.toInt()));
     return Scaffold(
       appBar: AppBar(),
-      body: Container(),
+      body: courseBuy.when(
+        data: (data) => WebView(
+          initialUrl: data,
+          javascriptMode: JavascriptMode.unrestricted,
+          javascriptChannels: {
+            JavascriptChannel(
+              name: 'Payment',
+              onMessageReceived: (JavascriptMessage message) {
+                print('------message received------');
+              },
+            ),
+          },
+          onWebViewCreated: (WebViewController controller) {
+            print('------created------');
+            webViewController = controller;
+          },
+        ),
+        error: (error, trace) => Text('Error getting webview'),
+        loading: () => const Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
     );
   }
 }
