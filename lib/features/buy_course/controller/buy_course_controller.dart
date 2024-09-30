7a84f39ce116e270158ef8f62c404d8d3e414122
Bullets-jyr <jyr@bullets.co.kr>
2024-09-30 18:49:04 +0900

    Build buy repo

--- lib/features/buy_course/controller/buy_course_controller.dart
+++ lib/features/buy_course/controller/buy_course_controller.dart
@@ -1,5 +1,6 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:ulearning_app/common/models/course_entities.dart';
+import 'package:ulearning_app/features/buy_course/repo/buy_course_repo.dart';
 import 'package:ulearning_app/features/course_detail/repo/course_repo.dart';
 
 part 'buy_course_controller.g.dart';
@@ -11,7 +12,7 @@ Future<String?> buyCourseController(
   CourseRequestEntity courseRequestEntity = CourseRequestEntity();
   courseRequestEntity.id = index;
   final response =
-  await CourseRepo.courseDetail(params: courseRequestEntity);
+  await BuyCourseRepo.buyCourse(params: courseRequestEntity);
   if (response.code == 200) {
     // return response.data;
     return null;
---
+++ lib/features/buy_course/repo/buy_course_repo.dart
@@ -0,0 +1,15 @@
+import 'package:ulearning_app/common/models/base_entities.dart';
+import 'package:ulearning_app/common/models/course_entities.dart';
+import 'package:ulearning_app/common/services/http_util.dart';
+
+class BuyCourseRepo {
+  static Future<BaseResponseEntity> buyCourse({
+    CourseRequestEntity? params,
+  }) async {
+    var response = await HttpUtil().post(
+      'api/checkout',
+      queryParameters: params?.toJson(),
+    );
+    return BaseResponseEntity.fromJson(response);
+  }
+}
