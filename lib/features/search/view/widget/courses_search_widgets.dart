6d318f30f0578658696f3daa0f5defdbf9f782ec
Bullets-jyr <jyr@bullets.co.kr>
2024-10-02 17:33:29 +0900

    Refactor code using global widget and ListView

--- lib/features/search/view/widget/courses_search_widgets.dart
+++ lib/features/search/view/widget/courses_search_widgets.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:ulearning_app/common/models/course_entities.dart';
 import 'package:ulearning_app/common/widgets/course_tile_widgets.dart';
 
-class CoursesBoughtWidgets extends StatelessWidget {
+class CoursesSearchWidgets extends StatelessWidget {
   final List<CourseItem> value;
 
-  const CoursesBoughtWidgets({
+  const CoursesSearchWidgets({
     super.key,
     required this.value,
   });
